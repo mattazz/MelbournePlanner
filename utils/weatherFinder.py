c77dd592859501da36c74bb96e4f5b19c6f108c7
Mattazz <matthewazada101@gmail.com>
2024-11-25 16:57:02 -0500

    successful api call, but need to build on weather forecast

--- utils/weatherFinder.py
+++ utils/weatherFinder.py
@@ -1,14 +1,16 @@
 from dotenv import load_dotenv  # using .env files
 import os  # Accessing files
+from pprint import pprint  ## pretty print. Makes prints pretty
 
 import weatherapi
+from weatherapi.rest import ApiException
 
 load_dotenv()  # checks for a .env file
 weather_api_key = os.getenv("WEATHER_API")
 
 # weather config
 configuration = weatherapi.Configuration()
-configuration.api_key = weather_api_key  # sets the api key
+configuration.api_key["key"] = weather_api_key  # sets the api key
 
 # http://api.weatherapi.com/v1/current.json?key=b49b00d1b6e24ae9875152543242409&q=Melbourne&aqi=no
 
@@ -25,4 +27,12 @@ dt = "2024-11-26"
 
 
 def get_weather_on_date():
-    return
+    try:
+        api_response = api_instance.forecast_weather(q, dt)
+        return api_response
+    except ApiException as e:
+        print("Exception when calling APIsApi -> Forecast: %s\n" % e)
+
+
+result = get_weather_on_date()
+pprint(result)
